acf98786dd1033449b3bbf4b5a99ef60e2a9254d
= <=>
2022-06-10 19:55:36 -0400

    fix: Estilo de navbar modificado

--- src/components/Pastillero/Navbar.js
+++ src/components/Pastillero/Navbar.js
@@ -1,15 +1,13 @@
 import React from "react";
-import "../../styles/pastillero.css";
+import "../../styles/navbar.css";
 
 export default function Navbar(props) {
     return(
         <div className="navbar">
-            <div className="content">
-                <button>Home</button>
-                <h1>Mi pastillero</h1>
-                <button>Search</button>
-                <button>Cart</button>
-            </div>
+            <button class="iconButton"><i class="fa fa-bars"></i></button>
+            <div className="title">Mi pastillero</div>
+            <button className="searchButton"><i class="fa fa-search" aria-hidden="true"></i></button>
+            <button className="cartButton"><i class="fa fa-shopping-cart" aria-hidden="true"></i></button>
         </div>
     )
 }
