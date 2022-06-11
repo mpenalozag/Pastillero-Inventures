81025daf03f1a82241abb507bb5863b1d8165e03
= <=>
2022-06-10 20:39:17 -0400

    feat: Componente de un medicamento creado con el nombre 'Medicine'

--- src/components/Pastillero/Container.js
+++ src/components/Pastillero/Container.js
@@ -4,13 +4,18 @@ import "../../styles/container.css";
 import Navbar from "./Navbar";
 import Description from "./Description";
 import Remaining from "./Remaining";
+import Medicine from "./Medicine";
 
 export default function Container(props) {
+    // TO DO: Hacer requests a la api para rellenar el pastillero del usuario.
+
+
     return(
         <div className="container">
             <Navbar />
             <Description />
             <Remaining />
+            <Medicine />
         </div>
     )
 }
--- src/components/Pastillero/Medicine.js
+++ src/components/Pastillero/Medicine.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "../../styles/medicine.css";
 
 export default function Medicine(props) {
     return(
         <div className="medicine">
-
+            <div className="medicinePhoto"></div>
+            <div className="medicineDetails">
+                <div className="medicineName">Something</div>
+                <div className="medicineDose">100mg</div>
+                <div className="medicineRemaining">Quedan 30 comprimidos</div>
+                <div className="medicineDaysLeft">Para 30 dias</div>
+            </div>
+            <button className="shoppingCart"><i class="fa fa-cart-plus" aria-hidden="true"></i></button>
         </div>
     )
 }
